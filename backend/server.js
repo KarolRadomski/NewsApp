e1ae3d8fe1bd5a0ecaac4f43da290a7cb4c69758
KarolRadomski <77892318+KarolRadomski@users.noreply.github.com>
2022-04-16 19:58:31 +0200

    second2

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,8 @@ if (process.env.NODE_ENV === 'production') {
       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     )
   );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 //Error handler
 app.use(errorHandler);
