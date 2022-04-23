47dd3dfb4ae5f0443bdf860a3028d6fee6142db9
KarolRadomski <77892318+KarolRadomski@users.noreply.github.com>
2022-04-23 20:43:40 +0200

    Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import style from '../styles/Footer.module.css';
+
+function Footer() {
+  return (
+    <div className={style.footer}>
+      <p className={style.footerText}>&copy; Copyright 2022 - Karol Radomski</p>
+    </div>
+  );
+}
+
+export default Footer;
